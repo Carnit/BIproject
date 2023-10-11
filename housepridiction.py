d1c6b23391d864487895e9b37266c6d14d675094
Carnit <99026097+Carnit@users.noreply.github.com>
2023-10-11 09:20:12 +0530

    update

--- housepridiction.py
+++ housepridiction.py
@@ -5,16 +5,16 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 import matplotlib.pyplot as plt
 data = pd.read_csv('dataset.csv')
-X = data[['bedrooms', 'S_footage', 'age']]  
+x = data[['bedrooms', 'S_footage', 'age']]  
 y = data['house_price'] 
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+x_train,x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
 model = LinearRegression()
 
-model.fit(X_train, y_train)
+model.fit(x_train, y_train)
 
-y_pred = model.predict(X_test)
+y_pred = model.predict(x_test)
 
 mse = mean_squared_error(y_test, y_pred)
 mae = mean_absolute_error(y_test, y_pred)
